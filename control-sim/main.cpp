663d85f0eb2040831d72ad4931bcdd1d0aceee00
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2021-11-08 16:47:15 +0000

    Added ability to open window

--- control-sim/main.cpp
+++ control-sim/main.cpp
@@ -1,8 +1,13 @@
 #include <MainGFX/Graphics.hpp>
 #include <stdio.h>
 
+GLFWwindow * window;
 
 int main() {
-    printf("hello world\n");
+    bool windowOpened;
+    if (!gfx::InitialiseGLFW()) return -1;
+    window = gfx::OpenWindow("Control Sim", windowOpened);
+    if (!windowOpened) return -1;
+    gfx::Main(window);
     return 0;
 }
